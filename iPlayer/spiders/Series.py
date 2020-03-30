c66ef0407859c82e4d01ce6b4bbe35e99facabbd
Charles de Freitas <charles@defreitas.io>
2020-03-30 18:54:34 +0100

    Add Series spider

---
+++ iPlayer/spiders/Series.py
@@ -0,0 +1,30 @@
+import scrapy
+from scrapy import Selector
+from scrapy.http import Response
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "spooks"
+
+    def start_requests(self):
+        urls = [
+            'https://www.bbc.co.uk/iplayer/episodes/b00mjlxv/merlin',
+            'https://www.bbc.co.uk/iplayer/episodes/b006mf4b/spooks'
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def get_episodes(self, response: Response):
+        for episode in response.css('ul.gel-layout li'):
+            print(f"https://bbc.co.uk/{episode.css('a').attrib['href']}")
+            yield {
+                'url': f"https://bbc.co.uk{episode.css('a').attrib['href']}"
+            }
+
+    def parse(self, response: Response):
+        series: Selector = response.css('nav.series-nav ul')
+        yield from self.get_episodes(response)
+        # Find all season links
+        for link in series.css('li a'):
+            yield scrapy.Request("https://bbc.co.uk" + link.attrib['href'], self.get_episodes)
